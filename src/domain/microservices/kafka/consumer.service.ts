4e608868788914b0d14de5596ab9db9db36734a6
Luis del Amo <luisdelamo@gmail.com>
2022-11-01 18:01:02 +0100

    added configuration

--- src/domain/microservices/kafka/consumer.service.ts
+++ src/domain/microservices/kafka/consumer.service.ts
@@ -9,7 +9,7 @@ import {
 @Injectable()
 export class ConsumerService implements OnApplicationShutdown {
   private readonly kafka = new Kafka({
-    brokers: ['localhost:9092'],
+    brokers: [],
   });
   private readonly consumers: Consumer[] = [];
 
