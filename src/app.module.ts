187815b373a842ecca247efb8bd0b6d1c3a05247
Luis del Amo <luisdelamo@gmail.com>
2022-11-01 18:00:37 +0100

    added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { MongodbModule } from './domain/infrastructure/database/mongodb/mongodb.module';
 import { HeroModule } from './heroes/hero.module';
 
 @Module({
-  imports: [HeroModule, MongodbModule],
+  imports: [ConfigModule.forRoot(), HeroModule, MongodbModule],
   controllers: [],
   providers: [],
 })
---
+++ src/config/environment.ts
@@ -0,0 +1,4 @@
+export enum ConfigEnv {
+  MONGO_URI = 'MONGO_URI',
+  KAFKA_URI = 'KAFKA_URI',
+}
